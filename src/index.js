1a6cfd95582978a8afc5054451005877b88801e8
saikoro-steak <saikoro-steak@example.com>
2019-08-15 14:13:52 +0900

    template some modules

--- src/index.js
+++ src/index.js
@@ -1,4 +1,10 @@
 import _ from 'lodash'
+import * as utilities from './utilities.js'
+import Lion from './utilities'
+
+console.log(NiJou(2));
+console.log(Lion.say());
+
 function component(){
   const element = document.createElement('div');
   const array = ['hello','webpack','8888888888888']
